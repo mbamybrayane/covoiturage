afe07c64aef0b28ba2b4002a59119cf3ca21e024
Lerob <149784528+mbamybrayane@users.noreply.github.com>
2025-07-13 10:24:51 +0100

    Update page.tsx

--- app/dashboard/driver/[userId]/page.tsx
+++ app/dashboard/driver/[userId]/page.tsx
@@ -1,12 +1,13 @@
 // app/dashboard/driver/[userId]/page.tsx
 
 import DriverDashboardClient from "./DriverDashboardClient";
-interface PageProps {
+
+export default function DriverDashboardPage({
+  params,
+}: {
   params: {
     userId: string;
   };
-}
-
-export default function DriverDashboardPage({ params }: PageProps) {
+}) {
   return <DriverDashboardClient userId={params.userId} />;
 }
